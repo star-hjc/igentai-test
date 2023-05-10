9e68da05d1ca3c472f05e05d39eaa221ba638662
hjc59476905 <89001850+hjc59476905@users.noreply.github.com>
2023-05-10 20:00:58 +0800

    fix:更新

--- electron/preload/mainPreload.js
+++ electron/preload/mainPreload.js
@@ -1,7 +1,8 @@
 const { contextBridge, ipcRenderer } = require('electron')
 const common = require('./modules/common')
-const adb = require('./modules/adb')
+const adbApi = require('./modules/adb')
 const view = require('./modules/window')
+const adb = {}
 
 contextBridge.exposeInMainWorld('appApi', {
     ...common,
@@ -9,7 +10,7 @@ contextBridge.exposeInMainWorld('appApi', {
 })
 
 contextBridge.exposeInMainWorld('adb', {
-    ...adb
+    ...adbApi
 })
 
 contextBridge.exposeInMainWorld('view', {
@@ -17,18 +18,25 @@ contextBridge.exposeInMainWorld('view', {
 })
 
 contextBridge.exposeInMainWorld('deviceCase', {
-    run: async (code) => {
-        return new Promise((resolve, reject) => {
-            try {
-                // eslint-disable-next-line no-eval
-                eval(`(async()=>{${code}})()`).then(res => {
+    run: async (code, device = {}) => {
+        for (const key in adbApi) {
+            adb[key] = adbApi[key].bind(this, device)
+        }
+
+        return new Promise(async (resolve, reject) => {
+            // eslint-disable-next-line no-eval
+            const codeData = await eval(`(async()=>{${code}})()`)
+                .then(res => {
                     resolve(true)
                 })
-            } catch (err) {
-                // eslint-disable-next-line no-console
-                console.error(err)
-                reject(false)
-            }
+                .catch(err => {
+                    // eslint-disable-next-line no-console
+                    console.error(err)
+                })
+            console.log(codeData)
+        }).catch(err => {
+            console.log(1231)
+            console.log(err)
         })
     }
 })
