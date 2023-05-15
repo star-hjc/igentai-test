4c511c16883969876ddd1ef63a94505e29ac6bc0
hjc59476905 <89001850+hjc59476905@users.noreply.github.com>
2023-05-15 21:02:24 +0800

    fix:更新

--- electron/preload/workBenchesPreload.js
+++ electron/preload/workBenchesPreload.js
@@ -20,6 +20,15 @@ contextBridge.exposeInMainWorld('run', (code, device) => {
     const adb = {}
     if (/.*require\(.*/.test(code)) return
     for (const key in adbApi) { adb[key] = adbApi[key].bind(this, device) }
+    // eslint-disable-next-line no-unused-vars
+    async function DOM () {
+        return new DOMParser().parseFromString(await adb.getUI(), 'text/xml')
+    }
+    // eslint-disable-next-line no-unused-vars
+    async function querySelector (str, num) {
+        if (!isNaN(num / 1)) return (await DOM()).querySelectorAll(str)[num]
+        return (await DOM()).querySelectorAll(str)
+    }
     // eslint-disable-next-line no-eval
     return eval(`(async()=>{${code}})()`)
 })
